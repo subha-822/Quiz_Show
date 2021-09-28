c41460b79ce6d2d468607b6142f86b63e78bae7a
Subha Rajagopal <subhagopal333@gmail.com>
2021-09-28 17:50:04 +0530

    first commit

---
+++ Javascript.js
@@ -0,0 +1,189 @@
+
+    function fn1()
+    {
+     document.getElementById("demo").innerHTML = "Correct Answer"
+     document.getElementById("demo").style.color ="Green"
+    }
+    function fn2()
+    {
+     document.getElementById("demo").innerHTML = "Wrong Answer"
+     document.getElementById("demo").style.color ="Red"
+    }
+
+    function fn3()
+    {
+     document.getElementById("demo1").innerHTML = "Correct Answer"
+     document.getElementById("demo1").style.color ="Green"
+    }
+    function fn4()
+    {
+     document.getElementById("demo1").innerHTML = "Wrong Answer"
+     document.getElementById("demo1").style.color ="Red"
+    }
+    function fn5()
+    {
+     document.getElementById("demo2").innerHTML = "Correct Answer"
+     document.getElementById("demo2").style.color ="Green"
+    }
+    function fn6()
+    {
+     document.getElementById("demo2").innerHTML = "Wrong Answer"
+     document.getElementById("demo2").style.color ="Red"
+    }
+    function fn7()
+    {
+     document.getElementById("demo3").innerHTML = "Correct Answer"
+     document.getElementById("demo3").style.color ="Green"
+    }
+    function fn8()
+    {
+     document.getElementById("demo3").innerHTML = "Wrong Answer"
+     document.getElementById("demo3").style.color ="Red"
+    }
+    function fn9()
+    {
+     document.getElementById("demo4").innerHTML = "Correct Answer"
+     document.getElementById("demo4").style.color ="Green"
+    }
+    function fn10()
+    {
+     document.getElementById("demo4").innerHTML = "Wrong Answer"
+     document.getElementById("demo4").style.color ="Red"
+    }
+   
+
+
+
+    function gfg_Run1() 
+    {
+
+     if(exampleCheck15.checked==true)
+     {
+       var radioButton = document.getElementById("exampleCheck15");
+       radioButton.checked = false;
+       el_down.innerHTML = "Unchecked";
+     }
+     else if(exampleCheck16.checked==true)
+     {
+       var radioButton = document.getElementById("exampleCheck16");
+       radioButton.checked = false;
+       el_down.innerHTML = "Unchecked";
+     }
+     else (exampleCheck17.checked==true)
+     {
+       var radioButton = document.getElementById("exampleCheck17");
+       radioButton.checked = false;
+       el_down.innerHTML = "Unchecked";
+     
+     }
+
+   }
+
+   function gfg_Run2() 
+    {
+
+     if(exampleCheck25.checked==true)
+     {
+       var radioButton = document.getElementById("exampleCheck25");
+       radioButton.checked = false;
+       el_down.innerHTML = "Unchecked";
+     }
+     else if(exampleCheck26.checked==true)
+     {
+       var radioButton = document.getElementById("exampleCheck26");
+       radioButton.checked = false;
+       el_down.innerHTML = "Unchecked";
+     }
+     else (exampleCheck27.checked==true)
+     {
+       var radioButton = document.getElementById("exampleCheck27");
+       radioButton.checked = false;
+       el_down.innerHTML = "Unchecked";
+     
+     }
+
+   }
+
+   function gfg_Run3() 
+   {
+
+    if(eg3.checked==true)
+    {
+      var radioButton = document.getElementById("eg3");
+      radioButton.checked = false;
+      el_down.innerHTML = "Unchecked";
+    }
+    else if(eg4.checked==true)
+    {
+      var radioButton = document.getElementById("eg4");
+      radioButton.checked = false;
+      el_down.innerHTML = "Unchecked";
+    }
+    else (eg5.checked==true)
+    {
+      var radioButton = document.getElementById("eg5");
+      radioButton.checked = false;
+      el_down.innerHTML = "Unchecked";
+    
+    }
+
+  }
+
+  function gfg_Run4() 
+  {
+
+   if(eg6.checked==true)
+   {
+     var radioButton = document.getElementById("eg6");
+     radioButton.checked = false;
+     el_down.innerHTML = "Unchecked";
+   }
+   else if(eg7.checked==true)
+   {
+     var radioButton = document.getElementById("eg7");
+     radioButton.checked = false;
+     el_down.innerHTML = "Unchecked";
+   }
+   else (eg8.checked==true)
+   {
+     var radioButton = document.getElementById("eg8");
+     radioButton.checked = false;
+     el_down.innerHTML = "Unchecked";
+   
+   }
+
+ }
+ function gfg_Run5() 
+ {
+
+  if(eg9.checked==true)
+  {
+    var radioButton = document.getElementById("eg9");
+    radioButton.checked = false;
+    el_down.innerHTML = "Unchecked";
+  }
+  else if(eg10.checked==true)
+  {
+    var radioButton = document.getElementById("eg10");
+    radioButton.checked = false;
+    el_down.innerHTML = "Unchecked";
+  }
+  else (eg11.checked==true)
+  {
+    var radioButton = document.getElementById("eg11");
+    radioButton.checked = false;
+    el_down.innerHTML = "Unchecked";
+  
+  }
+
+}
+  
+
+
+
+
+
+
+
+
+ 
